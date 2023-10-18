173ae2ae66e45f6074529e63c839401b16b5eef3
Naman1809 <72972766+Naman1809@users.noreply.github.com>
2023-10-18 10:00:43 +0530

    8:47

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./component/layout/Header/Header";
+import WebFont from "webfontloader";
+import Footer from "./component/layout/Footer/Footer";
+import Home from "./component/Home/Home";
+// import Loader from "./component/layout/Loader/Loader";
+import ProductDetails from "./component/Product/ProductDetails";
+import Products from "./component/Product/Products";
+import Search from "./component/Product/Search";
+import LoginSignUp from "./component/User/LoginSignUp";
+import store from "./store";
+import { loadUser } from "./actions/userAction";
+import UserOptions from "./component/layout/Header/UserOptions";
+import { useSelector } from "react-redux";
+import Profile from "./component/User/Profile";
+import ProtectedRoute from "./component/Route/ProtectedRoute";
+
+function App() {
+  const { isAuthenticated, user } = useSelector((state) => state.user);
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Router>
+      <Header />
+      {isAuthenticated && <UserOptions user={user} />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:keyword" element={<Products />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/login" element={<LoginSignUp />} />
+
+        <Route path="/" element ={<ProtectedRoute/>}>
+        <Route  path="/account" element={<Profile />} />
+
+        </Route>
+
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
